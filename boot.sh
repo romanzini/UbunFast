1db95dd380756591de59a4e27b6674fc5d9b1b7e
Rodrigo Romanzini <rodrigo.romanzini@gmail.com>
2025-04-16 14:28:21 -0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -8,17 +8,5 @@ echo -e "\nBegin installation (or abort with ctrl+c)..."
 sudo apt-get update >/dev/null
 sudo apt-get install -y git >/dev/null
 
-echo "Cloning Ubinkaze..."
-rm -rf ~/.local/share/ubunfast
-git clone https://github.com/romanzini/UbunFast.git ~/.local/share/ubunfast >/dev/null
-
-UBUNFAST_REF=${UBUNFAST_REF:-"stable"}
-
-if [[ $UBUNFAST_REF != "main" ]]; then
-  cd ~/.local/share/ubunfast
-  git fetch origin "$UBUNFAST_REF" && git checkout "$UBUNFAST_REF"
-  cd - >/dev/null
-fi
-
 echo "Installation starting..."
 source ~/.local/share/ubunfast/install.sh
